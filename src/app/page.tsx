078e535518e2df7cfebc3ded851570a9e97b5acb
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2024-03-18 18:15:21 -0500

    init shadcn + aceternity, add aurora background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { AuroraBackground } from "@/components/ui/aurora-background";
+
 export default async function Page() {
   return (
-    <>
-      <h1>Portfolio Website</h1>
-      <h1>Raghunath Prabhakar</h1>
-      <h1>Test</h1>
-    </>
+    <AuroraBackground>
+      <>
+        <h1>Raghunath Prabhakar</h1>
+      </>
+    </AuroraBackground>
   );
 }
