67925f31561b09ce79cbecf0cb34d3a03231d905
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2024-03-24 21:17:27 -0500

    fix typewriter styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,18 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="h-[40rem] w-full flex flex-col items-center justify-center overflow-hidden gap-10">
-      <div>
-        <TypewriterEffect
-          words={[
-            {
-              text: "Raghunath",
-            },
-            {
-              text: "Prabhakar",
-            },
-          ]}
-          className="text-9x1"
-          cursorClassName="bg-muted"
-        />
-      </div>
+      <TypewriterEffect
+        words={[
+          {
+            text: "Raghunath",
+          },
+          {
+            text: "Prabhakar",
+          },
+        ]}
+        className="flex items-end text-5xl sm:text-5xl md:text-5xl lg:text-5xl font-bold text-center"
+        cursorClassName="bg-muted h-full"
+      />
       <motion.div
         initial={{ y: "+100dvh", opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
