8de5351f24d0cefce8583c4e4424515dd0ec9470
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2024-03-25 22:29:56 -0500

    make animation smoother

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,21 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <div className="h-[40rem] w-full flex flex-col items-center justify-center overflow-hidden gap-10">
-      <TypewriterEffect
-        words={[
-          {
-            text: "Raghunath",
-          },
-          {
-            text: "Prabhakar",
-          },
-        ]}
-        className="flex items-end text-5xl sm:text-5xl md:text-5xl lg:text-5xl font-bold text-center"
-        cursorClassName="bg-muted h-full"
-      />
-      <HomeSubtitle />
+      <div className="flex flex-col gap-3">
+        <TypewriterEffect
+          words={[
+            {
+              text: "Raghunath",
+            },
+            {
+              text: "Prabhakar",
+            },
+          ]}
+          className="flex items-end text-5xl sm:text-5xl md:text-5xl lg:text-5xl font-bold text-center"
+          cursorClassName="bg-muted h-full"
+        />
+        <HomeSubtitle />
+      </div>
       <SocialMediaButtons />
     </div>
   );
--- src/features/home-subtitle.tsx
+++ src/features/home-subtitle.tsx
@@ -2,34 +2,54 @@
 import TextRotator from "@/components/ui/text-rotator";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1,
+      duration: 0.5,
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.5 },
+  },
+};
+
 const HomeSubtitle = () => {
   return (
-    <>
-      <div className="flex gap-3 items-center text-3xl">
-        <motion.div
-          className="flex items-center"
-          layout
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-        >
-          <TextRotator
-            words={[
-              "Full-Stack",
-              "Frontend",
-              "Backend",
-              "DevOps",
-              "Javascript",
-              "Python",
-              "Java",
-              "React",
-              "Node.js",
-              "Next.js",
-              "Nest.js",
-            ]}
-          />
-        </motion.div>
-        <p>Developer</p>
-      </div>
-    </>
+    <motion.div
+      className="flex gap-3 items-center text-3xl"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      layout
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+    >
+      <TextRotator
+        words={[
+          "Full-Stack",
+          "Frontend",
+          "Backend",
+          "DevOps",
+          "Javascript",
+          "Python",
+          "Java",
+          "React",
+          "Node.js",
+          "Next.js",
+          "Nest.js",
+        ]}
+      />
+      <motion.p variants={childVariants}>Developer</motion.p>
+    </motion.div>
   );
 };
 
