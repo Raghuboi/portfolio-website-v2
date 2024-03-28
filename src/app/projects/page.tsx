53f0af5276dd2ee1cac1f457f8b780f0ea0a6d13
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2024-03-27 20:35:42 -0500

    export empty component to resolve build error

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+interface Props {}
+
+const Page: React.FC<Props> = () => {
+  return (
+    <div className="h-[40rem] w-full flex justify-center items-center overflow-hidden px-[3rem]">
+      <h1>Coming soon!</h1>
+    </div>
+  );
+};
+
+export default Page;
