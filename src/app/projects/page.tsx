0eabc22ab8a1ae24f2143b72d2c3d06acf9d68aa
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2024-03-29 13:48:48 -0500

    add step for generating env

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ interface Props {}
 const Page: React.FC<Props> = () => {
   return (
     <div className="h-[40rem] w-full flex justify-center items-center overflow-hidden px-[3rem]">
-      <h1>Coming soon!!!</h1>
+      <h1>Coming Soon!</h1>
     </div>
   );
 };
